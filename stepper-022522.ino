f84bd9d672d1e0f3f6c6424ef6ca479742b604b7
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-02-25 03:38:28 -0800

    Create stepper-022522.ino

---
+++ stepper-022522.ino
@@ -0,0 +1,52 @@
+#include <Stepper.h>
+
+// nema 17 bipolar motor with 200 steps per revolution draws 1.7 A at 2.8 V,
+// microstepping 1/16 is 2.5 microns per step
+
+const int stepsPerRevolution = 200;  // change this the steps/rev for your motor
+
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+void setup() {
+  // initialize the serial port:
+  Serial.begin(9600);
+}
+
+
+int stepCount = 0;         // number of steps the motor has taken
+// moves 40 microns per step
+// pitch length to move to
+int pitchint = float (39.0) / float (0.04);
+
+
+void loop() {
+  // step one step:
+  myStepper.step(1);
+  Serial.print("steps:");
+  Serial.println(stepCount);
+  stepCount++;
+  delay(500);
+  
+  myStepper.step(1);
+  Serial.print("steps:");
+  Serial.println(stepCount);
+  stepCount++;
+  delay(500);
+  
+  myStepper.step(1);
+  Serial.print("steps:");
+  Serial.println(stepCount);
+  stepCount++;
+  delay(500);
+  
+  myStepper.step(-3);
+  Serial.print("steps:");
+  Serial.println(stepCount);
+  stepCount++;
+  delay(500);
+  
+  
+  if (stepCount > 6){
+    stepper.stop();}
+  
+}
