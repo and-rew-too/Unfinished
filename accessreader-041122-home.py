5d949375e00974274fd032d1d34dea5164a42e68
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-04-11 21:38:47 -0700

    Update accessreader-041122-home.py

--- accessreader-041122-home.py
+++ accessreader-041122-home.py
@@ -6,12 +6,14 @@ import pyodbc
 
 
 #test dataframe
-df = pd.DataFrame(np.zeros((5,20)))
 
 
 import pygsheets
 gc = pygsheets.authorize(
     service_file='C:/Users/andre/Downloads/fluent-outlet-329800-d7ce5f1f4cd1.json')
 sh = gc.open('2022 Reliability Parts') #open the google spreadsheet
+print(df)
 wks = sh[1]
-wks.set_dataframe(df, start=(1,1))
+
+wks.set_dataframe(df, start=(1,1),copy_head=False)
+
