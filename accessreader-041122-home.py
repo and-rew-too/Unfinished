f797452d41fa1b776ff5736137da7f447c9f721e
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-04-11 21:36:56 -0700

    Update accessreader-041122-home.py

--- accessreader-041122-home.py
+++ accessreader-041122-home.py
@@ -1,4 +1,17 @@
 import pyodbc
 
 
+#THEN AFTER THIS ALL OF THE access database has then been turned into a google sheets
+#pass the dataframe through, into uhh 
 
+
+#test dataframe
+df = pd.DataFrame(np.zeros((5,20)))
+
+
+import pygsheets
+gc = pygsheets.authorize(
+    service_file='C:/Users/andre/Downloads/fluent-outlet-329800-d7ce5f1f4cd1.json')
+sh = gc.open('2022 Reliability Parts') #open the google spreadsheet
+wks = sh[1]
+wks.set_dataframe(df, start=(1,1))
