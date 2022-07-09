72e5225e6c64930c4b0288578697e2db5a4ac042
and-rew-too <93184013+and-rew-too@users.noreply.github.com>
2022-07-09 00:15:23 -0700

    Update AH-accessreader-3.py

--- AH-accessreader-3.py
+++ AH-accessreader-3.py
@@ -8,3 +8,13 @@ PPtPresentation = PptApp.Presentations.Open(r'C:\Users\andre\Downloads\Test.pptx
 pptSlide = PPtPresentation.Slides.Add(1,11)
 Pict1 = pptSlide.Shapes.AddPicture(FileName=r'C:\Users\andre\Desktop\mytable.png', LinkToFile=False, SaveWithDocument=True, Left=100, Top=100, Width=-1, Height=-1)
 
+pptSlide3 = PPtPresentation.Slides.Add(1,11)
+pptSlide5 = PPtPresentation.Slides.Add(1,11)
+Pict2 = pptSlide3.Shapes.AddPicture(FileName=r'C:\Users\andre\Desktop\mytable.png', LinkToFile=False, SaveWithDocument=True, Left=100, Top=100, Width=-1, Height=-1)
+#still ends up creating, 1 blank title slide, 2 slides with dataframe pictures, and then the last slide which is also blank
+
+pptSlide3 = PPtPresentation.Slides.Add(1,11)
+Pict2 = pptSlide3.Shapes.AddPicture(FileName=r'C:\Users\andre\Desktop\mytable.png', LinkToFile=False, SaveWithDocument=True, Left=100, Top=100, Width=-1, Height=-1)
+txBox = pptSlide.Shapes.AddTextbox(1,100,100,400,400)
+tf = txBox.TextFrame
+Final = tf.TextRange.Text = "Here is some test text"
